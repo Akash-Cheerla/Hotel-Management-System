e87796e5faeb7df758791e7b49bd7968cafc0691
AKASH CHEERLA <akashcvs007@gmail.com>
2019-03-04 10:31:38 +0530

    Add files via upload

---
+++ Project/SQl files/hotel.sql
@@ -0,0 +1,16 @@
+
+create table teaches (hotel_id varchar(5) not null,
+hotel_name varchar(20) not null, 
+hotel_type varchar(8) not null,
+hotel_rent varchar(12) not null,
+Primary key(hotel_id), 
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+hotel_id,
+hotel_name,
+hotel_type,
+hotel_rent,
+hotel_description,
+hotel_address
---
+++ Project/SQl files/insert_hotel.sql
@@ -0,0 +1,5 @@
+insert into hotel values ('SB101', 'Baskey Hotel', '5 star hotel', 'bhuwaneshwar', 'bhuwaneshwar,orissa');
+insert into hotel values ('SH101', 'Hansda Hotel', '5 star hotel', 'Jamshedpur', 'jamshedpur,jharkhand');
+insert into customer values ('JJ101', 'Sachin Hansda', '1222222', 'Jam@sachin.co', 'jamshedpur,jharkhand');
+insert into customer values ('TE111', 'Akash Cheerla', '234567', 'akash@ymail.co', 'telangana');
+insert into customer values ('BO101', 'Surendra Baskey', '1233445', 'sb@gmail.com', 'orissa');
